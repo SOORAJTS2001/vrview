9f140609a16bf5872896258aaa7cc2deb86670e8
SOORAJ TS <72636628+SOORAJTS2001@users.noreply.github.com>
2023-08-29 19:54:32 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
     // Selector '#vrview' finds element with id 'vrview'.
     var vrView = new VRView.Player('#vrview', {
-        image: "https://64ed-2409-4073-414-19f-2488-3916-8473-2a94.ngrok-free.app/upscaled_image/upscaled_image.png",
+        image:"https://cors-anywhere.herokuapp.com/" + "https://64ed-2409-4073-414-19f-2488-3916-8473-2a94.ngrok-free.app/upscaled_image/upscaled_image.png",
     });
 
     vrView.on('click', function (event) {
